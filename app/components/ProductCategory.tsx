a0fcdbc1bc3512ea5712de048372b9047d428038
SurajToraskar <toraskarsurajsuryakant@gmail.com>
2025-09-20 16:34:19 +0530

    completed adding text and alignment

---
+++ app/components/ProductCategory.tsx
@@ -0,0 +1,15 @@
+export default function ProductCategory() {
+  return (
+    <div className="flex justify-center items-center mt-[180px]">
+      <div className="text-center">
+        <h1 className="font-sans text-[50px]">Quality Products</h1>
+        <p className="text-gray-400 mt-6 text-md w-full max-w-xl break-words">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Banner from "./components/Banner";
+import ProductCategory from "./components/ProductCategory";
+import "./globals.css";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <ProductCategory />
     </>
   );
 }
