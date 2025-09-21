6377c5181ab417ba2c6b317f5cf168f832856948
SurajToraskar <toraskarsurajsuryakant@gmail.com>
2025-09-22 00:02:55 +0530

    added animation to text

--- app/components/ProductCategory.tsx
+++ app/components/ProductCategory.tsx
@@ -7,13 +7,22 @@ export default function ProductCategory() {
 
   return (
     <div className="flex justify-center items-center mt-[180px] p-4">
-      <div
-        ref={ref}
-        className={`text-center transition-all duration-1000 ease-out
-        ${isInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}
-      >
-        <h1 className="font-sans text-[50px]">Quality Products</h1>
-        <p className="text-gray-400 mt-6 text-md w-full max-w-xl break-words">
+      <div ref={ref} className="text-center">
+        <h1
+          className={`font-sans text-[50px] transition-all duration-1000 ease-out
+          ${
+            isInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+          }`}
+        >
+          Quality Products
+        </h1>
+
+        <p
+          className={`text-gray-400 mt-6 text-md w-full max-w-xl break-words transition-all duration-1000 ease-out delay-700
+          ${
+            isInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+          }`}
+        >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
